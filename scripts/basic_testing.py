be82be5f0e2cc14c6645b92376bd566b106e5f07
Dakota <dakotawinslow@gmail.com>
2025-04-18 22:42:15 -0400

    Added scripts

---
+++ scripts/basic_testing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+from pylimo import limo
+import time
+
+limo = limo.LIMO()
+limo.EnableCommand()  # 使能控制s
+num = 10
+while num > 0:
+    limo.SetMotionCommand(linear_vel=1, steering_angle=20)
+    time.sleep(0.3)
+    num -= 1
+
+quit()
