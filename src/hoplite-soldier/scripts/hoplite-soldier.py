5e7bf9a15f9dae22c7a3f025cdf0b0253e1fd962
winslowd <winslowd@bu.edu>
2025-05-05 12:11:50 -0400

    tuning

--- src/hoplite-soldier/scripts/hoplite-soldier.py
+++ src/hoplite-soldier/scripts/hoplite-soldier.py
@@ -220,7 +220,7 @@ class HopliteKinematicModel(object):
         # PD controller parameters
         self.linear_kp = 1.0  # Proportional gain for linear velocity
         self.linear_kd = 0.1  # Derivative gain for linear velocity
-        self.angular_kp = 1.0  # Proportional gain for angular velocity
+        self.angular_kp = 0.1  # Proportional gain for angular velocity
         self.angular_kd = 0.5  # Derivative gain for angular velocity
 
         # Deadzones
